47c50598c2fc18472ffa04ab7b460f30b7b2134f
Albert Klinkovský <albert.klinkovsky@gmail.com>
2020-11-24 18:57:43 +0100

    Cleaning the source file

--- test.py
+++ test.py
@@ -6,22 +6,21 @@ import unittest
 import subprocess
 
 
-def get_set(name: str) -> list:
+EXECUTABLE = "../build/sps"
+
+
+def get_set() -> dict:
 	with open("set.json") as file:
-		return json.loads(file.read())[name]
+		return json.loads(file.read())
 
 
 class TestSPreadSheet(unittest.TestCase):
-
-	EXECUTABLE = "../build/sps"
-
-	def _run_test_io(self, data):
+	def _run_test_io(self, data, name):
 		for itext, cmd, otext, rcode in data:
 			with tempfile.NamedTemporaryFile() as tf:
-				with self.subTest(cmd=[self.EXECUTABLE] + cmd + [tf.name], itext=itext, otext=otext, rcode=rcode):
+				with self.subTest(name=name, cmd=[EXECUTABLE] + cmd + [tf.name], itext=itext, otext=otext, rcode=rcode):
 					tf.write(itext.encode('utf-8'))
 					tf.seek(0)
-					print(tf.name)
 					r = subprocess.run(
 						[self.EXECUTABLE] + cmd + [tf.name],
 						encoding="utf-8",
@@ -30,29 +29,10 @@ class TestSPreadSheet(unittest.TestCase):
 					self.assertEqual(r.returncode, rcode, f"Result code must be: {rcode}")
 					self.assertEqual(tf.read(), otext)
 
-	def _run_test_result(self, data):
-		for itext, cmd, otext, rcode in data:
-			with self.subTest(cmd=cmd, itext=itext, otext=otext, rcode=rcode):
-				r = subprocess.run(
-					[self.EXECUTABLE] + cmd,
-					input=itext,
-					encoding="utf-8",
-					capture_output=True
-				)
-				self.assertEqual(r.returncode, rcode, f"Result code must be: {rcode}")
-
-	def test_result(self):
-		data = get_set('results')
-		self._run_test_result(data)
-
-	def test_delimiter(self):
-		pass
-
-	def test_cell_selection(self):
-		""" Test Cell Selection """
-
-		data = get_set('cell_selection')
-		self._run_test_io(data)
+	def test_io(self):
+		data = get_set()
+		for key in data:
+			self._run_test_io(data[key], key)
 
 
 if __name__ == "__main__":
